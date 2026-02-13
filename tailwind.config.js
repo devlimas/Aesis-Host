d367332b439a1c501bf5ac5d3613db7117a073a8
devlimas <lima01dev@gmail.com>
2026-02-13 07:35:25 -0300

    feat: adição do tailwind nativo e remoção do script tailwind via html

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      height: {
+      '121': '30.25rem',
+      },
+      margin: {
+        '15': '3.75rem',
+      },
+    },
+  },
+  plugins: [],
+}
+
